29e4a837a590bb32cde195fbe6f5c871e84de100
Dev. Ralph <raphaelakpor00@gmail.com>
2023-11-26 14:16:47 +0100

    animation added

---
+++ src/script.js
@@ -0,0 +1,47 @@
+// let dropDownToggler = document.getElementById('menu');
+// let list = document.getElementById('list');
+
+// dropDownToggler.addEventListener('click', showMenu);
+
+// function showMenu() {
+//   if (list.className.includes('show')) {
+//     list.classList.remove('show');
+//   } else {
+//     list.classList.add('show');
+//     // dropDownToggler.innerHTML = '<ion-icon name="close"></ion-icon>';
+//   }
+// }
+
+// const recipient = 'raphaelakpor00@gmail.com';
+// const emailSubjectInput = document.getElementById('emailSubject');
+// const emailContentTextarea = document.getElementById('emailContent');
+// const sendEmailButton = document.getElementById('sendEmail');
+
+// sendEmailButton.addEventListener('click', sendMail);
+
+// function sendMail() {
+//   const subject = emailSubjectInput.value;
+//   const content = emailContentTextarea.value;
+
+//   // Create a "mailto" URL with the subject and body parameters
+//   const mailtoURL = `mailto:${encodeURIComponent(recipient)}
+//   ?subject=${encodeURIComponent(subject)}
+//   &body=${encodeURIComponent(content)}`;
+
+//   // Open the user's default email application with the "mailto" URL
+//   window.location.href = mailtoURL;
+// }
+
+// const observer = new IntersectionObserver((entries) => {
+//   entries.forEach((entry) => {
+//     console.log(entry);
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('show');
+//     } else {
+//       entry.target.classList.remove('show');
+//     }
+//   });
+// });
+
+// const hiddenElements = document.querySelectorAll('.hidden');
+// hiddenElements.forEach((el) => observer.observe(el));
